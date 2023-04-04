51aca31f97b85b759eab3d3e0d5943067172b028
shatong <843391622@qq.com>
2023-04-04 15:50:53 +0800

    feat:添加实时绘制逻辑

--- src/components/canvas.tsx
+++ src/components/canvas.tsx
@@ -1,48 +1,72 @@
 import { useEffect, useRef, useState } from "react";
 import { Socket } from "socket.io-client";
 
-export function Canvas({client}:{client:Socket|null}) {
+export function Canvas({ client }: { client: Socket | null }) {
   const canvasRef = useRef<HTMLCanvasElement>(null);
-  let [ctx, setCtx] = useState<CanvasRenderingContext2D|null>(null);
+  let [ctx, setCtx] = useState<CanvasRenderingContext2D | null>(null);
 
   useEffect(() => {
     const canvas = canvasRef.current;
     let w = 0;
     let h = 0;
-    if(canvas){
+    if (canvas) {
       canvas.width = w = canvas.clientWidth;
       canvas.height = h = canvas.clientHeight;
-      if(!ctx){
+      if (!ctx) {
         setCtx(canvas.getContext('2d'))
       }
     }
-    client?.on("draw", ({x,y,w,h}) => {
-      if(ctx){
-        ctx.fillStyle = '#23a5cd';
-        ctx.fillRect(x, y, w, h);
+    client?.on("draw", ({ type, x, y }) => {
+      switch (type) {
+        case 'begin':
+          if (ctx) {
+            ctx.fillStyle = '#23a5cd';
+            ctx.strokeStyle = '#23a5cd'
+            ctx.beginPath();
+          }
+          break;
+        case 'draw':
+          ctx?.lineTo(x, y);
+          ctx?.stroke();
+          break;
       }
     });
     return () => {
       client?.off("draw")
     }
-  },[ctx,client])
+  }, [ctx, client])
 
-  const onMouseDown = (e:any) => {
-    if(ctx){
+  const onMouseDown = (e: any) => {
+    const canvas = canvasRef.current;
+    canvas?.addEventListener('mousemove', onMouseMove)
+    if (ctx) {
       ctx.fillStyle = '#23a5cd';
-      ctx.fillRect(e.clientX-e.target.offsetLeft, e.clientY-e.target.offsetTop, 100, 100);
-      client?.emit("draw",{
-        x:e.clientX-e.target.offsetLeft,
-        y:e.clientY-e.target.offsetTop,
-        w:100,
-        h:100
+      ctx.strokeStyle = '#23a5cd'
+      ctx.beginPath();
+      client?.emit("draw", {
+        type: 'begin'
       })
     }
   }
-  
+
+  const onMouseUp = () => {
+    const canvas = canvasRef.current;
+    canvas?.removeEventListener('mousemove', onMouseMove)
+  }
+
+  const onMouseMove = (e: any) => {
+    ctx?.lineTo(e.clientX - e.target.offsetLeft, e.clientY - e.target.offsetTop)
+    ctx?.stroke();
+    client?.emit("draw", {
+      type: 'draw',
+      x: e.clientX - e.target.offsetLeft,
+      y: e.clientY - e.target.offsetTop,
+    })
+  }
+
   return (
     <>
-      <canvas ref={canvasRef} className="h-full w-full rounded bg-white" onMouseDown={onMouseDown}></canvas>
+      <canvas ref={canvasRef} className="h-full w-full rounded bg-white" onMouseDown={onMouseDown} onMouseUp={onMouseUp}></canvas>
     </>
   )
 }
